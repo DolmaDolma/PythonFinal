0b36a765914a8ba68c82b4e844546fb9633c3098
DolmaDolma <91643156+DolmaDolma@users.noreply.github.com>
2022-01-13 12:53:33 -0500

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,33 @@
+class Question:
+  def __init__(self, text , answer):
+    self.text = text   # text and answer attributes
+    self.answer = answer
+
+
+
+
+# #When a  funciton is attached to an objkect it is called method 
+
+
+# class User:
+#   def __init__(self, user_id, username):
+#     self.id = user_id
+#     self.username = username
+#     self.followers = 0
+#     self.following =0
+
+#   def follow(self,user):
+#     user.followers += 1
+#     self.following += 1
+#     #self.id is an attribute associated with the class
+# user_1 = User("001", "Dolma")
+# user_2 =User("002","Sonu")
+# # print(user_2.id)
+# # print(user_2.username)
+# # print(user_2. followers)
+# user_1.follow(user_2)
+
+# print(user_1.followers)
+# print(user_1.following)
+
+  
